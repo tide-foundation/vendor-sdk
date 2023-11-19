61d62f4816086a3507be24f0a8f429cc93917575
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2023-11-19 17:53:25 +1000

    added auth.verifyjwt method

---
+++ Vendor SDK/Auth.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TinySDK.Ed25519;
+using TinySDK.Math;
+using TinySDK.Models;
+using TinySDK.Tools;
+using Vendor_SDK.Models;
+
+namespace Vendor_SDK
+{
+    public class Auth
+    {
+        public static bool VerifyJwt(string jwt, string tidePublicKey)
+        {
+            // Verify jwt and it's timestamp
+            var JWT = new TideJWT(jwt, true); 
+            var pub = Point.FromBase64(tidePublicKey);
+            if (!EdDSA.Verify(JWT.GetDataToSign(), JWT.signature, pub)) return false;
+            long epochNow_seconds = Utils.GetEpochSeconds();
+            if (JWT.payload.exp <= epochNow_seconds) return false;
+            return true;
+        }
+    }
+}
